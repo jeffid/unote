0994fc34c1ce07dba758ec04fd8975dc0eb72577
jeffid <jeffid@tuta.io>
2024-10-30 15:49:15 +0800

    - The share_handler package applies to ios devices

---
+++ ios/ShareExtension/ShareViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by JF on 2024/10/30.
+//
+
+import share_handler_ios_models
+    
+class ShareViewController: ShareHandlerIosViewController {}
